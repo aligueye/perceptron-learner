824bf8676c00d38b9a4a4a999743fa1db987f3f0
Corbin Grosso <grossoc2@login002.cm.cluster>
2021-05-06 03:16:36 -0400

    Implemented graphs; made dataset shuffling its own function so the order can more easily be output; code for every task is now implemented and executed all at once

--- model.py
+++ model.py
@@ -3,56 +3,54 @@
 # Description: Perceptron model
 
 import random
+import matplotlib.pyplot as plt
+
 
 class Model:
 
-    def __init__(self, flower):
+    def __init__(self, flower, initial_weight_value=0.0, random_weights=False):
         """
 		Initialize unlearned model.
 
 		args:
-			flower : the model's flower type, either iris setosa, iris versicolor, or iris virginica
+			flower: the model's flower type, either iris setosa, iris versicolor, or iris virginica
+            initial_weight_value: Initial value for the weights. Ignored if random_weights=False
+            random_weights: Initalizes weights to random value between 1-0. If passed False, initialize all weights to 1
 		"""
-        self.flower = flower
-        self.init_weights(4)
-        self.errors = 0
-        self.epochs = 0
 
-    def init_weights(self, num_attributes, random_weights = False):
+        self.flower = flower
+        self.init_weights(4, initial_weight_value, random_weights)
+        self.epochs = 0
+        self.errors_after_training = 0
+        self.errors_at_each_epoch = []
+
+    def init_weights(self, num_attributes, initial_weight_value=0.0, random_weights=False):
         """
         Initialize weights of unlearned model
 
         args:
             num_attributes: Number of attributes in each training example
-            random: Initalizes weights to random value between 1-0. If passed False, initialize all weights to 1
+            initial_weight_value: Initial value for the weights. Ignored if random_weights=True
+            random_weights: Initalizes weights to random value between 1-0. If passed False, initialize all weights to 1
         """
+
         if random_weights:
             self.weights = [random.random() for i in range(num_attributes + 1)]
         else:
             self.weights = [0.0 for i in range(num_attributes + 1)]
 
-    def train(self, examples, shuffle = False):
+    def train(self, examples, learning_rate=0.01):
         """
-        Trains model with provided data. Algorithm implemented is Stochastic Gradient Descent (SGD)
+        Trains model with provided data. Algorithm implemented is the Perceptron Training Rule
+
         args:
-            shuffle: Determines if data should be shuffles. If passed True, shuffle training data randomly
+            examples: List of training examples
+            learning_rate: Rate at which the model learns
         """
 
-        # Randomly shuffles examples
-        if shuffle:
-            random.shuffle(examples)
-
-        errors_in_epoch = 1 # set to 1 to assure training loop executes
-        errors_in_prev = 0
-        errors_in_prev2 = 0
-
-        while (errors_in_epoch > 0) ^ (errors_in_prev2 == errors_in_epoch):
-            if(self.epochs != 0):
-                errors_in_prev2 = errors_in_prev
-                errors_in_prev = errors_in_epoch
-
+        while True:
             self.epochs += 1
-            errors_in_epoch = 0
+            self.errors_at_each_epoch.append(0)
 
             for example in examples:
                 target = self.classify(example)
@@ -60,15 +58,25 @@ class Model:
 
                 if target is not output: # Weights are only changed if prediction is incorrect
                     
-                    self.errors += 1
-                    errors_in_epoch +=1
+                    self.errors_at_each_epoch[-1] += 1
 
+                    # Update weights
                     for i in range(len(example) - 1):
-                        self.weights[i] = self.weights[i] + (0.01 * (target - output) * example[i])
-            print(f"Weights: {self.weights}")
-            print(f"Epochs: {self.epochs}")
-            print(f"Errors {errors_in_epoch}")
-            print(f"errors_in_prev = errors_in_epoch: {errors_in_prev == errors_in_epoch}")
+                        self.weights[i] = self.weights[i] + (learning_rate * (target - output) * example[i])
+            
+            print(f"Epoch: {self.epochs}")
+            print(f"Amount of Errors on Current Epoch: {self.errors_at_each_epoch[-1]}")
+            print(f"Current Wight Vector: {self.weights}")
+            print(self.errors_at_each_epoch)
+            
+            # if no errors are made in the most recent epoch
+            if self.errors_at_each_epoch[-1] == 0:
+                break
+            # if there are more than one epoch completed and the newest two have the same number of errors
+            if self.epochs > 1 and self.errors_at_each_epoch[-1] == self.errors_at_each_epoch[-2]:
+                break
+            
+        self.errors_after_training = self.errors_at_each_epoch[-1]
 
 
     def classify(self, training_example):
@@ -77,10 +85,12 @@ class Model:
         args:
             training_example: List of attributes to use to classify the flower type
         """
+
         if self.flower == training_example[-1]:
             return 1
         else:
             return -1
+
 
     def predict(self, training_example):
         """
@@ -98,8 +108,41 @@ class Model:
         else:
             return -1
     
+
     def to_string(self):
+        """
+        Prints out all of the information about the model
+        """
+
         print(f"Target flower: {self.flower}")
         print(f"Weights: {self.weights}")
         print(f"Epochs: {self.epochs}")
-        print(f"Errors {self.errors}")
+        print(f"Errors: {self.errors_after_training}")
+
+
+    def graph_errors_versus_epochs(self, output_directory):
+        """
+        Creates a graph of the number of errors vs the number of epochs.
+        Saves the graph in the specified output directory.
+
+        args:
+            output_directory: Path to a file to save the graph as
+        """
+
+        if self.epochs == 0:
+            raise ValueError("The model has not been trained.\n\
+                The model must be trained before a graph can be produced.")
+
+        print(self.epochs)
+        print([i for i in range(self.epochs)])
+        print(self.errors_at_each_epoch)
+        
+        plt.plot([i for i in range(1, self.epochs + 1)], self.errors_at_each_epoch)
+        plt.locator_params(axis="both", integer=True, tight=True)
+        plt.title('Errors Made Vs. Epochs')
+        plt.xlabel('Epochs')
+        plt.ylabel('Amount of Errors')
+        plt.ylim(bottom=0)
+        plt.savefig(output_directory)
+        plt.clf()
+
--- project4.py
+++ project4.py
@@ -2,34 +2,200 @@
 # Filename: project4.py
 
 from model import Model
+import random
 
-# reads data
+
+def shuffle_data(output_directory):
+    """
+    Shuffles the data in the dataset and records it in the specified output file
+
+    args:
+        output_directory: path to a file to record all of the shuffled data
+    """
+
+    random.shuffle(dataset)
+    with open(output_directory, 'w') as f:
+        for example in dataset:
+            line = ''
+            for attr in example[1:]:
+                line += f"{attr},"
+            line = line[:-1] # remove trailing comma
+            line += '\n'
+            f.write(line)
+
+
+def read_in_data(input_directory):
+    """
+    Reads in data from the specified input file and formats it to be used for training
+
+    args:
+        input_directory: path to a file containing data to use for training
+    """
+
+    # reads data
+    with open(input_directory,'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        dataset.append(line.split(','))
+
+    # format data
+    for row in dataset:
+        row[-1] = row[-1].strip()
+        for i in range(4):
+            row[i] = float(row[i])
+        row.insert(0, 1) # Makes the first value 1, pushes every other attribute over by 1, represents w_0
+
+
 dataset = []
-file = open('iris.data','r')
-for line in file:
-    dataset.append(line.split(','))
-file.close()
 
-# format data
-for row in dataset:
-    row[-1] = row[-1].strip()
-    for i in range(4):
-        row[i] = float(row[i])
-    row.insert(0, 1) # Makes the first value 1, pushes every other attribute over by 1, represents w_0
+read_in_data('iris.data')
 
-# Task 1: Trains model for three different learning problems
-model_1_1 = Model('Iris-setosa')
-model_1_1.train(dataset)
-model_1_1.to_string()
+# Naming scheme used for the model objects is
+# model_{name of target flower}_{task the model is for, with decimals replaced with underscores}
+
+# Task 2: Trains model for three different learning problems
+print("Task 2:\n")
+model_setosa_2 = Model('Iris-setosa')
+model_setosa_2.train(dataset)
+model_setosa_2.to_string()
+model_setosa_2.graph_errors_versus_epochs('graphs/task_2_setosa.pdf')
 
 print()
 
-model_1_1 = Model('Iris-versicolor')
-model_1_1.train(dataset)
-model_1_1.to_string()
+model_versicolor_2 = Model('Iris-versicolor')
+model_versicolor_2.train(dataset)
+model_versicolor_2.to_string()
+model_versicolor_2.graph_errors_versus_epochs('graphs/task_2_versicolor.pdf')
 
 print()
 
-model_1_1 = Model('Iris-virginica')
-model_1_1.train(dataset)
-model_1_1.to_string()
+model_virginica_2 = Model('Iris-virginica')
+model_virginica_2.train(dataset)
+model_virginica_2.to_string()
+model_virginica_2.graph_errors_versus_epochs('graphs/task_2_virginica.pdf')
+
+print()
+
+
+# Task 3.1: Trains models with initial weights all set to 1
+print("Task 3.1:\n")
+model_setosa_3_1 = Model('Iris-setosa', 1)
+model_setosa_3_1.train(dataset)
+model_setosa_3_1.to_string()
+model_setosa_3_1.graph_errors_versus_epochs('graphs/task_3.1_setosa.pdf')
+
+print()
+
+model_versicolor_3_1 = Model('Iris-versicolor', 1)
+model_versicolor_3_1.train(dataset)
+model_versicolor_3_1.to_string()
+model_versicolor_3_1.graph_errors_versus_epochs('graphs/task_3.1_versicolor.pdf')
+
+print()
+
+model_virginica_3_1 = Model('Iris-virginica', 1)
+model_virginica_3_1.train(dataset)
+model_virginica_3_1.to_string()
+model_virginica_3_1.graph_errors_versus_epochs('graphs/task_3.1_virginica.pdf')
+
+print()
+
+
+# Task 3.2: Trains models with four random weights between 0 and 1
+print("Task 3.2:\n")
+model_setosa_3_2 = Model('Iris-setosa', random_weights=True)
+model_setosa_3_2.train(dataset)
+model_setosa_3_2.to_string()
+model_setosa_3_2.graph_errors_versus_epochs('graphs/task_3.2_setosa.pdf')
+
+print()
+
+model_versicolor_3_2 = Model('Iris-versicolor', random_weights=True)
+model_versicolor_3_2.train(dataset)
+model_versicolor_3_2.to_string()
+model_versicolor_3_2.graph_errors_versus_epochs('graphs/task_3.2_versicolor.pdf')
+
+print()
+
+model_virginica_3_2 = Model('Iris-virginica', random_weights=True)
+model_virginica_3_2.train(dataset)
+model_virginica_3_2.to_string()
+model_virginica_3_2.graph_errors_versus_epochs('graphs/task_3.2_virginica.pdf')
+
+print()
+
+
+# Task 3.3: Trains models with four random weights between 0 and 1
+print("Task 3.3:\n")
+model_setosa_3_3 = Model('Iris-setosa', random_weights=True)
+model_setosa_3_3.train(dataset)
+model_setosa_3_3.to_string()
+model_setosa_3_3.graph_errors_versus_epochs('graphs/task_3.3_setosa.pdf')
+
+print()
+
+model_versicolor_3_3 = Model('Iris-versicolor', random_weights=True)
+model_versicolor_3_3.train(dataset)
+model_versicolor_3_3.to_string()
+model_versicolor_3_3.graph_errors_versus_epochs('graphs/task_3.3_versicolor.pdf')
+
+print()
+
+model_virginica_3_3 = Model('Iris-virginica', random_weights=True)
+model_virginica_3_3.train(dataset)
+model_virginica_3_3.to_string()
+model_virginica_3_3.graph_errors_versus_epochs('graphs/task_3.3_virginica.pdf')
+
+print()
+
+
+# Task 4.1: Trains model for three different learning problems
+shuffle_data('iris_shuffle_for_T4.1.data')
+
+print("Task 4.1:\n")
+model_setosa_4_1 = Model('Iris-setosa')
+model_setosa_4_1.train(dataset)
+model_setosa_4_1.to_string()
+model_setosa_4_1.graph_errors_versus_epochs('graphs/task_4.1_setosa.pdf')
+
+print()
+
+model_versicolor_4_1 = Model('Iris-versicolor')
+model_versicolor_4_1.train(dataset)
+model_versicolor_4_1.to_string()
+model_versicolor_4_1.graph_errors_versus_epochs('graphs/task_4.1_versicolor.pdf')
+
+print()
+
+model_virginica_4_1 = Model('Iris-virginica')
+model_virginica_4_1.train(dataset)
+model_virginica_4_1.to_string()
+model_virginica_4_1.graph_errors_versus_epochs('graphs/task_4.1_virginica.pdf')
+
+print()
+
+
+# Task 4.2: Trains model for three different learning problems
+shuffle_data('iris_shuffle_for_T4.2.data')
+
+print("Task 4.2:\n")
+model_setosa_4_2 = Model('Iris-setosa')
+model_setosa_4_2.train(dataset)
+model_setosa_4_2.to_string()
+model_setosa_4_2.graph_errors_versus_epochs('graphs/task_4.2_setosa.pdf')
+
+print()
+
+model_versicolor_4_2 = Model('Iris-versicolor')
+model_versicolor_4_2.train(dataset)
+model_versicolor_4_2.to_string()
+model_versicolor_4_2.graph_errors_versus_epochs('graphs/task_4.2_versicolor.pdf')
+
+print()
+
+model_virginica_4_2 = Model('Iris-virginica')
+model_virginica_4_2.train(dataset)
+model_virginica_4_2.to_string()
+model_virginica_4_2.graph_errors_versus_epochs('graphs/task_4.2_virginica.pdf')
+
+print()
